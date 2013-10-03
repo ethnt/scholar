f313e4924cfbfaff005b99af6867a0c9d789badb
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2013-10-03 13:48:05 -0400

    initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Scholar
+
+Scholar is learning management made simple.
+
+## Objectives
+
+* **Keep all resources self-contained** &mdash; Student and course information needs to be secure. Most universities require this information to stay on-campus, and not be hosted by third-parties.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scholar::Application.config.secret_key_base = '9272c80c93fde15d503912c8006d8189be4308caee5d8fcf4403c29ba0d8bd01b189ccdd90639a31afd479e99adc04d633ae8679dc3aa68701c339a12c43478e'
